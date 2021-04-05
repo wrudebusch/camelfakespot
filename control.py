a022f04c859634bb4511c6fb8c1e659bd5611555
will <wrudebusch@gmail.com>
2021-04-04 21:08:08 -0400

    new logic

--- control.py
+++ control.py
@@ -14,9 +14,9 @@ pg_port = os.environ.get("PG_PORT")
 pg_user = os.environ.get("PG_USER")
 
 os.system("python3 popular_chrome.py")
-os.system("python3 top_drops_chrome.py")
+os.system("python3 topdrops_chrome.py")
 
-for name in ["top_drops", "popular"]:
+for name in ["topdrops", "popular"]:
     os.system(f"python3 any_csv_to_table.py {name}")
     os.remove(name + ".csv")
 
