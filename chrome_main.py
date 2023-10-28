3196172bf237675340e2f7d836e4a74f1c964778
will <wrudebusch@gmail.com>
2023-10-28 18:12:39 -0400

    edits

--- chrome_main.py
+++ chrome_main.py
@@ -9,7 +9,6 @@ from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
